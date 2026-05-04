e5973572c0f322fd236f360ba0de33b96060ce4c
Valentin <valentinlinevic1@gmail.com>
2026-05-04 19:31:55 +0300

    init commit

---
+++ src/main/java/com/duikt/hotelroomsreservations/HotelRoomsReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.duikt.hotelroomsreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomsReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelRoomsReservationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duikt/hotelroomsreservations/HotelRoomsReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duikt.hotelroomsreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomsReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
